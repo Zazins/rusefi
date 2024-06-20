6d17a1ed1786a821d77d07b1a52143c9c422e16f
Zazins <86603174+Zazins@users.noreply.github.com>
2024-06-20 12:14:45 +0300

    Update can_dash.cpp

--- firmware/controllers/can/can_dash.cpp
+++ firmware/controllers/can/can_dash.cpp
@@ -144,9 +144,12 @@ static void canDashboardBmwE46(CanCycle cycle) {
             CanTxMessage msg(CanCategory::NBC, CAN_BMW_E46_RPM);
             int rpmValue = (int)(Sensor::getOrZero(SensorType::Rpm) * 6.4);
             msg.setShortValue(rpmValue, 2);
-            msg.setShortValue(0x050C, 0);  // Combine bitfield and Indexed Engine Torque
-            msg.setShortValue(0x0C00, 4);  // Combine Indicated Engine Torque and not used byte
-            msg.setShortValue(0x1535, 6);  // Combine Engine Torque Loss and Theoretical Engine Torque
+            msg.setShortValue(0x05, 0);  // Combine bitfield and Indexed Engine Torque
+            msg.setShortValue(0x0C, 1);  // Combine bitfield and Indexed Engine Torque
+            msg.setShortValue(0x0C, 4);  // Combine Indicated Engine Torque and not used byte
+            msg.setShortValue(0x15, 5);
+            msg.setShortValue(0x00, 6);  // Combine Engine Torque Loss and Theoretical Engine Torque
+            msg.setShortValue(0x35, 7);
         }
 
         // CAN_BMW_E46_DME2 message
@@ -154,17 +157,24 @@ static void canDashboardBmwE46(CanCycle cycle) {
             CanTxMessage msg(CanCategory::NBC, CAN_BMW_E46_DME2);
             int cltValue = (int)((Sensor::getOrZero(SensorType::Clt) + 48.373) / 0.75);
             msg.setShortValue(cltValue, 1);
-            msg.setShortValue(0x1100, 0);  // Combine Multiplexed Information and not used byte (was Baro)
-            msg.setShortValue(0x0800, 3);  // Combine bitfield (Clutch and Engine status) and TPS_VIRT_CRU_CAN (not used)
-            msg.setShortValue(0x0000, 5);  // Combine TPS (TDO) and bitfield (Brake and system status)
+            msg.setShortValue(0x11, 0);  // Combine Multiplexed Information and not used byte (was Baro)
+            msg.setShortValue(0x00, 2);
+            msg.setShortValue(0x08, 3);  // Combine bitfield (Clutch and Engine status) and TPS_VIRT_CRU_CAN (not used)
+            msg.setShortValue(0x00, 4);
+            msg.setShortValue(0x00, 5);  // Combine TPS (TDO) and bitfield (Brake and system status)
+            msg.setShortValue(0x00, 6);
+            msg.setShortValue(0x00, 7);
         }
 
         // CAN_BMW_E46_DME4 message
         {
             CanTxMessage msg(CanCategory::NBC, CAN_BMW_E46_DME4);
-            msg.setShortValue(0x0000, 0);  // Combine Multiplexed Information and Fuel Consumption LSB
-            msg.setShortValue(0x0000, 2);  // Combine Fuel Consumption MSB and Overheat light
-            msg.setShortValue(0x7E00, 4);  // Combine Oil Temp and not used byte
+            msg.setShortValue(0x00, 0);  // Combine Multiplexed Information and Fuel Consumption LSB
+            msg.setShortValue(0x00, 1);
+            msg.setShortValue(0x00, 2);  // Combine Fuel Consumption MSB and Overheat light
+            msg.setShortValue(0x00, 3);
+            msg.setShortValue(0x7E, 4);  // Combine Oil Temp and not used byte
+           
         }
     }
 }
